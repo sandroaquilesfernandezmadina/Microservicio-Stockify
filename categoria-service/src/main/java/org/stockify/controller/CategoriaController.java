17eb46a9c22c6c25698d8891e9e71fade3657f77
SaMFernandez <fsandroaquiles@gmail.com>
2026-01-23 12:24:21 -0500

    implementacion del CRUD completo en el controlador del microservicio de categoria-service

--- categoria-service/src/main/java/org/stockify/controller/CategoriaController.java
+++ categoria-service/src/main/java/org/stockify/controller/CategoriaController.java
@@ -1,13 +1,12 @@
 package org.stockify.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.stockify.entity.Categoria;
 import org.stockify.service.CategoriaService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categoria")
@@ -21,4 +20,25 @@ public class CategoriaController {
         return categoriaService.Findall();
     }
 
+    @GetMapping("/{id}")
+    public Optional <Categoria> findById(@PathVariable Long id){
+        return  categoriaService.FindById(id);
+    }
+
+    @PostMapping
+    public  Categoria save(@RequestBody Categoria categoria){
+        return categoriaService.Save(categoria);
+    }
+
+    @PutMapping("/{id}")
+    public Categoria update(@PathVariable Long id, @RequestBody Categoria categoria){
+    categoria.setId(id);
+    return categoriaService.Update(id, categoria);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        categoriaService.FindById(id);
+    }
 }
